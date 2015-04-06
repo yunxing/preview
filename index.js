84bb5860692f39a8b3344aafbf6f8ea817d9d242
Yunxing Dai <yunxing@upthere.com>
2015-04-06 13:33:04 -0700

    first

---
+++ index.js
@@ -0,0 +1,27 @@
+gen = function() {
+  var file = document.getElementById('filebox').files[0];
+  reader = new FileReader();
+  reader.readAsDataURL(file);
+  console.log(reader);
+  reader.onload = function() {
+    var img = new Image();
+    img.onload = function() {
+      var canvas = document.getElementById("canvas");
+      canvas.width = 300;
+      canvas.height = canvas.width * img.height / img.width;
+      var ctx = canvas.getContext("2d");
+      ctx.drawImage(img,0,0, canvas.width, canvas.height);
+      var before = (new Date).getTime();
+      compressed_data = canvas.toDataURL("image/jpeg", 0.3);
+      var after = (new Date).getTime();
+      console.log("time taken: " + (after - before) + " ms");
+      console.log("compress ratio: " + (compressed_data.length/reader.result.length * 100).toFixed(2) + "%");
+      var log = document.querySelector('[data-log]');
+      log.innerHTML = "size before: " + reader.result.length + " bytes</br>";
+      log.innerHTML = log.innerHTML + "size after: " + compressed_data.length + " bytes</br>";
+      log.innerHTML = log.innerHTML + "time taken: " + (after - before) + " ms</br>";
+      log.innerHTML = log.innerHTML + "compress ratio: " + (compressed_data.length/reader.result.length * 100).toFixed(2) + "%</br>";
+    };
+    img.src = reader.result;
+  }
+}
