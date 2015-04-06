e05a503213c8f95ff63207a274e104cfb704ea2e
Yunxing Dai <yunxing@upthere.com>
2015-04-06 14:06:41 -0700

    fix timing

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ gen = function() {
   reader.onload = function() {
     var img = new Image();
     img.onload = function() {
+      var before = (new Date).getTime();
+      console.log("before " + before);
       var canvas = document.getElementById("canvas");
-      canvas.width = 300;
+      canvas.width = 500;
       canvas.height = canvas.width * img.height / img.width;
       var ctx = canvas.getContext("2d");
       ctx.drawImage(img,0,0, canvas.width, canvas.height);
-      var before = (new Date).getTime();
       compressed_data = canvas.toDataURL("image/jpeg", 0.3);
       var after = (new Date).getTime();
       console.log("time taken: " + (after - before) + " ms");
